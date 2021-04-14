95298403731fbf50e97e0be3fb409fd1b144356b
Matthias Leitl <a.dead.trousers@gmail.com>
2021-04-14 19:18:50 +0200

    Splitting repositories for EEA / TEE support

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+-include device/Unihertz/Atom_L/BoardConfigCommon.mk
+
+DEVICE_REGION_PATH := device/Unihertz/Atom_L_TEE
+
+# Kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_REGION_PATH)/prebuilt/Image.gz
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_REGION_PATH)/prebuilt/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_REGION_PATH)/prebuilt/dtb
